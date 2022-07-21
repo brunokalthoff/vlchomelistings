cff032661b970521be52f59b30dd1619effaff0e
ethbru <ethbru@outlook.de>
2022-07-21 14:56:46 +0800

    Add email form endpoint

--- components/home/Contact.js
+++ components/home/Contact.js
@@ -1,17 +1,35 @@
 import styles from '../../styles/home/Contact.module.css'
+import { useState } from 'react';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
+import { motion } from 'framer-motion';
 
 function Contact() {
+const [sent, setSent] = useState(false);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setSent(true);
+        setTimeout(() => {
+            e.target.reset();
+            setSent(false);
+        }, 2000);
+    }
+
+
     return (
         <div className={styles.container}>
             <h2>Contact us 👋</h2>
             <div className={styles.formWrapper}>
                 <h3>Send a message. <span>We reply within 30 minutes - often sooner, and never automated.</span></h3>
                 <div className={styles.formCardWrapper}><div className={styles.card}>
-                    <form className={styles.form}>
-                        <div className={styles.input}><label htmlFor="name">Name</label><input name='name' type="text" /></div>
-                        <div className={styles.doubleInput}><div className={styles.input}><label htmlFor="email">Email</label><input name='email' type="text" /></div><div className={styles.input}><label htmlFor="phone">Phone</label><input name='phone' type="text" /></div></div>
-                        <div className={styles.input}><label htmlFor="note">Note</label><textarea name="note" rows="5"></textarea></div>
-                        <button><a>Send</a></button>
+                    <form onSubmit={handleSubmit} className={styles.form} name="Contact" netlify>
+                        <div className={styles.input}><label htmlFor="name">Name*</label><input name='name' type="text" required /></div>
+                        <div className={styles.doubleInput}><div className={styles.input}><label htmlFor="email">Email*</label><input name='email' type="email" required /></div><div className={styles.input}><label htmlFor="phone">Phone</label><input name='phone' type="text" /></div></div>
+                        <div className={styles.input}><label htmlFor="note">Note*</label><textarea name="note" rows="4" required></textarea></div>
+                        <button className={styles.button} type='submit' >
+                            {sent && <motion.div animate={{ rotate: 720 }} transition={{ duration: 3, repeat: Infinity, ease: "linear" }}><AiOutlineLoading3Quarters /></motion.div>}
+                            Send
+                        </button>
                     </form>
                 </div>
                 </div>
