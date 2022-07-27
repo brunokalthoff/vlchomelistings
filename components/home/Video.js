06f14afb34199bf7e343935973329b313b8d9be5
ethbru <ethbru@outlook.de>
2022-07-28 01:06:19 +0800

    Add fullscreen on doubleclick

--- components/home/Video.js
+++ components/home/Video.js
@@ -1,5 +1,5 @@
 import styles from '../../styles/home/Video.module.css'
-import { IoPlay, IoPause } from "react-icons/io5";
+import { FaPlay,FaPause } from "react-icons/fa";
 import { useRef, useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
@@ -32,16 +32,16 @@ function Video() {
     return (
         <div className={styles.videoWrapper}>
             <div className={styles.video} onClick={handleClick}>
-                <video ref={playerRef} width="100%" height="100%">
+                <video ref={playerRef} onDoubleClick={e=>e.target.requestFullscreen()} width="100%" height="100%">
                     <source src="/hero-video.mp4" type="video/mp4" />
                     <source src="/hero-video.ogg" type="video/ogg" />
                     <source src="/hero-video.webm" type="video/webm" />
                     Your browser does not support the video tag.
                 </video>
                 <motion.div className={styles.playPause}>
-                    {playing === 'init' && <IoPlay />}
-                    {!playing === true && <motion.span initial={{ opacity: 1 }} animate={{ opacity: 0 }} transition={{ duration: 1 }}><IoPause /></motion.span>}
-                    {playing && <motion.span initial={{ opacity: 1 }} animate={{ opacity: 0 }} transition={{ duration: 1 }}><IoPlay /></motion.span>}
+                    {playing === 'init' && <FaPlay />}
+                    {!playing === true && <motion.span initial={{ opacity: 1 }} animate={{ opacity: 0 }} transition={{ duration: 1 }}><FaPause /></motion.span>}
+                    {playing && <motion.span initial={{ opacity: 1 }} animate={{ opacity: 0 }} transition={{ duration: 1 }}><FaPlay /></motion.span>}
                 </motion.div>
             </div>
         </div>
