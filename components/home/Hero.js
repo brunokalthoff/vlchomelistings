824bf552cfb707b18679cbe27a61103a402c1b23
ethbru <ethbru@outlook.de>
2022-08-28 03:00:30 +0800

    video playspeed reduce

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -4,6 +4,7 @@ import { useRef, useEffect } from 'react';
 function Hero() {
     const videoOne = useRef();
     useEffect(() => {
+        videoOne.current.playbackRate = 0.9;
         videoOne.current.play();
     }, []);
 
@@ -11,7 +12,7 @@ function Hero() {
         <div className={styles.videoWrapper}>
             <div className={styles.video}>
 
-                <video poster="/videos/missionvision-placeholder.jpg" ref={videoOne} width="100%" height="100%" autoPlay muted loop>
+                <video poster="/videos/missionvision-placeholder.jpg" ref={videoOne} width="100%" height="100%" muted loop>
                     <source src="/videos/missionvision.webm" type="video/webm" />
                     <source src="/videos/missionvision.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
